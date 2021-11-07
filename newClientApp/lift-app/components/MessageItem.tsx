12b85aed46599f817eedc9eeaad38801765de35e
Tomas Radionovas <Tomas.Radionovas@ktu.edu>
2021-11-07 19:36:18 +0200

    adding some front end functionality

---
+++ newClientApp/lift-app/components/MessageItem.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import {
+	Flex,
+	Text,
+	IconButton,
+	Divider,
+	Avatar,
+	Heading
+} from '@chakra-ui/react'
+
+interface IMessageProps {
+	name:string | undefined;
+	message:string;
+	time:string;
+}
+export const MessageItem: React.FC<IMessageProps> = (props: IMessageProps) => {
+	return (
+		<Flex mt={1} align="center">
+			<Avatar size="sm" src="avatar-1.jpg" />
+			<Flex flexDir="column" ml={4} display={"flex"}>
+				<span style={{ display: "flex" }}>
+					<Heading color="white" as="h3" size="sm">{props.name}</Heading>
+					<Heading fontSize="11px" color="#9b9797" style={{ marginTop: "6px" }} ml={2} as="h3" size="xs">{new Date(props.time).toUTCString()}</Heading>
+				</span>
+
+				<Text fontSize="14px" color="#DCDAD9">{props.message}</Text>
+			</Flex>
+		</Flex>
+
+
+	)
+}
